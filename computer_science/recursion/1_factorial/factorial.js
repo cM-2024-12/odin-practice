d40537d7203d7a243a3a50eb3c3955ecefe75dba
Christian Merrick <192653753+cM-2024-12@users.noreply.github.com>
2026-02-14 18:09:51 -0500

    Update NPM - First Test Recursion

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,23 @@
-const factorial = function() {
-
+const factorial = function(n) {
+    // First: Check if input is valid (is it a number? is it an integer? is it non-negative?)
+    if (typeof n != 'number' || !Number.isInteger(n) || n < 0) {
+        return undefined
+    }
+    else if (n == 0) {
+        return 1
+    }
+    else {
+        return n * factorial(n-1)
+    }
+    // If invalid, return undefined
+    
+    // Second: Handle the base case (when n === 0)
+    // Return 1
+    
+    // Third: Handle the recursive case
+    // Return n * factorial(n-1)
 };
 
+factorial(4);
 // Do not edit below this line
 module.exports = factorial;
